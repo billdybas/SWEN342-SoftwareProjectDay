73d223a07cc6785b7ff258fc9b7a31115f6e09ed
Tina Howard <chh2538@rit.edu>
2016-10-17 10:31:46 -0400

    Added enums to the workday

--- Workday.java
+++ Workday.java
@@ -1,4 +1,14 @@
 
 public class Workday {
+	//We will have an enum of the various parts of the day
+	// This will contain the barrier of the the entrance and exits, and
+	// the barrier for the full workday
+	public enum times {
+
+		WORKDAY(4800),
+		HOUR(600),
+		MINUTE(10)
+
+	}
 
 }
